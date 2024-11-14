722c4f0d183b9d9a6d41fc9c36d33242ad7d1ef4
Colin Delehanty <colin.delehanty@gmail.com>
2024-11-14 13:57:01 -0800

    fix: removed SpeedInsights wrapper

--- src/components/speed-insights-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/SpeedInsightsWrapper.tsx
-import React from 'react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
-
-const SpeedInsightsWrapper: React.FC = () => {
-	return <SpeedInsights />
-}
-
-export default SpeedInsightsWrapper
